a1c61eb038512e425d865c43535401736c8c27df
ElijahMPalmer <palmerelijah@rocketmail.com>
2022-04-13 12:11:16 -0700

    Created basic paths in server for testing

--- src/server/server.js
+++ src/server/server.js
@@ -4,11 +4,29 @@ const bcrypt = require('bcrypt');
 const { Pool } = require('pg');
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(express.json());
+
+const pool = new Pool({
+    database: 'rapusers',
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 
 app.get('/', (req, res) => {
+    pool.query('SELECT * FROM users;', async(err, result) => {
+        res.send(result);
+        if (await bcrypt.compare(passkey, result.rows[0].passkey)) {
+            console.log('success');
+            res.send("Success");
+        } else {
+            console.log('access denied')
+            res.send("denied");
+        }
+    })
+})
+
+app.get('/login/:username/:password', (req, res) => {
     res.send('Hello World!')
 })
 
