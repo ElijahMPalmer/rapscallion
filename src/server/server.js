f99cc37723ff8a26c5047279bba614ea57027316
ElijahMPalmer <palmerelijah@rocketmail.com>
2022-04-13 11:51:45 -0700

    Set up Migration

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
+const { Pool } = require('pg');
 const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
