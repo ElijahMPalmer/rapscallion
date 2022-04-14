9dbdfebfceb0ce85af0c4cb2396870e515db27a7
ElijahMPalmer <palmerelijah@rocketmail.com>
2022-04-14 11:40:17 -0700

    Added Backend directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-const styles = require('./App.css');
-const axios = require('axios');
-const React = require('react');
-const Header = require('./components/Header.jsx');
+import './App.css';
+import axios from 'axios';
+import Header from './components/Header';
+import React from 'react';
 //Your API key is: RfNibr7lLoJZ9SKS6mJShB2MUCLGW2Zuza31kkb9swM=
 
 
