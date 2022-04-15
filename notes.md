6d2ed52e14807cdc2f19843c22f8b08d417b7a20
ElijahMPalmer <palmerelijah@rocketmail.com>
2022-04-15 12:18:20 -0700

    Init commit for jobBoard branch

---
+++ notes.md
@@ -0,0 +1,5 @@
+In order to merge branch with conflicts!!
+
+Git pull from master
+Git merge 'branch you want to merge' --no-ff rapscallion/'branch you want to merge'
+
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import axios from 'axios';
 import Header from './components/Header';
 import Home from './components/Home';
+import Job from './components/Job';
 import React from 'react';
 //Your API key is: RfNibr7lLoJZ9SKS6mJShB2MUCLGW2Zuza31kkb9swM=
 
